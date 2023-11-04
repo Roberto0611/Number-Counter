9a011f475844e324ad2c3467dc7d98ac1aae6218
Roberto0611 <ochoacuevasr@gmail.com>
2023-11-04 16:33:23 -0700

    final commit

--- main.js
+++ main.js
@@ -1,2 +1,29 @@
 //Script by: Roberto Ochoa Cuevas :)
+
+//set number and get counter
 var num = 0;
+var counter = document.getElementById('counter');
+
+//set text
+counter.textContent = num;
+
+//get increase and decrease buttons
+increaseButton = document.getElementById('increase');
+decreaseButton = document.getElementById('decrease');
+
+//increase Function
+function increase(){
+    num++ ; //this add 1 to the actual number
+    counter.textContent = num;
+}
+
+//Decrease Function
+function decrease(){
+    num--; //this substract 1 to the actual number
+    counter.textContent = num;
+}
+
+//EventListeners
+increaseButton.addEventListener("click", increase);
+decreaseButton.addEventListener("click", decrease);
+
